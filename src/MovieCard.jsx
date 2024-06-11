6f4411610a08066525069866feb15073aa0f83bb
Andrew Chu <andrewchu@meta.com>
2024-06-11 16:21:55 -0700

    search

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard({ title, poster_path, vote_average }) {
     return (
         <article className="movie-card">
             <h3 className="title">{title}</h3>
-            <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} className="poster" />
+            {poster_path && <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} className="poster" />}
             <h3>{vote_average}</h3>
         </ article>
     );
@@ -13,7 +13,7 @@ function MovieCard({ title, poster_path, vote_average }) {
 
 MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
-    poster_path: PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
     vote_average: PropTypes.number.isRequired,
 }
 
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -3,38 +3,119 @@ import MovieCard from './MovieCard';
 import './MovieList.css';
 
 
+
 function MovieList() {
-    const [movies, setMovies] = useState(undefined);
+    const [nowPlayingMovies, setNowPlayingMovies] = useState(undefined);
+    const [nowPlayingPage, setNowPlayingPage] = useState(1);
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchMovies, setSearchMovies] = useState(undefined);
+    const [searchPage, setSearchPage] = useState(0);
+
+    const [isSearchActive, setIsSearchActive] = useState(false);
 
     useEffect(() => {
         const options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
+                Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
             }
         };
 
-        const fetchData = async () => {
-            await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-                .then(response => response.json())
-                .then(response => setMovies(response))
-                .catch(err => console.error(err));
-        }
+        async function fetchData() {
+            try {
+                let url = "";
+                let page = 0;
+                if (isSearchActive) {
+                    page = searchPage;
+                    url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=${page}`;
+                } else {
+                    page = nowPlayingPage;
+                    url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`;
+                }
 
+                const response = await fetch(url, options);
+                const data = await response.json();
+                if (!data || !data.results) {
+                    throw new Error("Failed to fetch TMDB data.");
+                }
+
+                const updateMoviesList = (prevData) => {
+                    if (page === 1) {
+                        return data;
+                    } else if (prevData && prevData.results) {
+                        const existingIds = new Set(prevData.results.map(item => item.id));
+                        const filteredNewResults = data.results.filter(item => !existingIds.has(item.id));
+                        return {
+                            ...prevData,
+                            results: [...prevData.results, ...filteredNewResults]
+                        };
+                    }
+                    return prevData;
+                }
+
+                if (isSearchActive) {
+                    setSearchMovies(updateMoviesList);
+                } else {
+                    setNowPlayingMovies(updateMoviesList);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
         fetchData();
-    }, []);
+    }, [nowPlayingPage, searchPage, searchQuery]);
+
+    const handleSearchSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+        setSearchQuery(formData.get("query"));
+
+        setSearchPage(1);
+    }
+
+    const handleSearchToggle = () => {
+        if (isSearchActive) {
+            // returning to "now playing" page
+            setSearchQuery("");
+            setSearchMovies(undefined);
+            setSearchPage(0);
+
+            setNowPlayingPage(1);
+        }
+        setIsSearchActive(state => !state);
+    }
 
     return (
-        <section className='movie-list'>
-            {movies?.results.map(
-                movie =>
-                    <MovieCard key={movie.title}
-                        title={movie.title}
-                        poster_path={movie.poster_path}
-                        vote_average={movie.vote_average} />
-            )}
-        </section>
+        <>
+            <button onClick={handleSearchToggle}>Toggle</button>
+
+            {isSearchActive &&
+                <form onSubmit={handleSearchSubmit}>
+                    <input type="text" placeholder="Search" name="query" />
+                    <button type="submit">X</button>
+                </form>
+            }
+
+            <section className='movie-list'>
+                {(isSearchActive ? searchMovies : nowPlayingMovies)?.results.map(
+                    movie =>
+                        <MovieCard key={movie.id}
+                            title={movie.title}
+                            poster_path={movie.poster_path}
+                            vote_average={movie.vote_average} />
+                )}
+                {(searchPage > 0 || !isSearchActive) &&
+                    (<button onClick={() => {
+                        const setPage = isSearchActive ? setSearchPage : setNowPlayingPage;
+                        setPage(page => page + 1);
+                    }}>Load More</button>)
+                }
+            </section>
+        </>
     );
 }
 
